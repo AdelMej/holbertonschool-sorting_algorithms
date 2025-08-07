0837951dc558f53b39718fd01cfc2e48c836ee61
AdelMej <adelmejrissi@gmail.com>
2025-08-07 16:14:40 +0200

    fix: should 11/12 but i'm losing it bad now

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -73,8 +73,11 @@ static void quick_sort_printing(int *array,
 			i++;
 		}
 	}
-	swap(&array[pivot], &array[i]);
-	print_array(original_array, max_size);
+	if (i != pivot)
+	{
+		swap(&array[pivot], &array[i]);
+		print_array(original_array, max_size);
+	}
 	/*
 	 * recursively sort left size
 	 * start at the begining of the array
