236f561fab385625731c2510ddec262f3dfd9fc1
AdelMej <adelmejrissi@gmail.com>
2025-08-07 14:47:36 +0200

    feat: now has betty doc

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,8 +1,16 @@
 #include "sort.h"
 #include <stddef.h>
 
-void swap(int *a, int *b);
+static void swap(int *a, int *b);
 
+/**
+ * selection_sort - a function that sort an array
+ * using selection sort algorithm
+ * @array: an array to sort
+ * @size: the size of the array to sort
+ *
+ * Return: void
+ */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, swap_idx;
@@ -15,7 +23,7 @@ void selection_sort(int *array, size_t size)
 		swap_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if(array[swap_idx] > array[j])
+			if (array[swap_idx] > array[j])
 				swap_idx = j; /* change stored index */
 		}
 		if (i != swap_idx) /* if we need to swap */
@@ -25,8 +33,14 @@ void selection_sort(int *array, size_t size)
 		}
 	}
 }
-
-void swap(int *a, int *b)
+/**
+* swap - a function to swap 2 values
+* @a: a pointer to an integer to swap
+* @b: a pointer to an integer to swap
+*
+* Return: void
+*/
+static void swap(int *a, int *b)
 {
 	int temp = *a;
 	*a = *b;
